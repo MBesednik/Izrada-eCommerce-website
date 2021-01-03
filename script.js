e50024c2cf3ba13f20a11ff4c417ba3c76c29ee6
MBesednik <m.besednik69@gmail.com>
2021-01-03 23:46:31 +0100

    Dodavanje funkcionalnosti pri promjeni slika na stranici pojedinačnog proizvoda

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function menuToggle() {
         MenuItems.style.maxHeight = "0px";
     }
 }
+// js za slike ispod glavne slike proizvoda
+let productImg = document.getElementById("productImg");
+let smallImg = document.getElementsByClassName("small-img");
+smallImg[0].onclick = function () {
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function () {
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function () {
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function () {
+    productImg.src = smallImg[3].src;
+}
